a5c51fbe95e3d3747dedf76b80c385644ef9fcca
driquelme-esri-chile <driquelme@esri.cl>
2022-04-28 01:15:13 -0400

    Creando ventana dictica1 - aun mutando

---
+++ codigo-mutando.py
@@ -0,0 +1,56 @@
+"""Codigo no funcional, falta que mute"""
+
+
+from asyncore import write
+import datetime
+import pandas as pd
+import locale
+
+
+############################################
+############################################
+############################################
+
+import tkinter
+from tkinter import messagebox
+
+
+
+############################################
+############################################
+############################################
+
+from docxtpl import DocxTemplate
+from itertools import groupby
+from operator import itemgetter
+from asyncore import write
+import datetime
+import pandas as pd
+import locale
+from openpyxl import load_workbook
+import pandas.io.formats.excel
+from openpyxl.styles.alignment import Alignment
+from openpyxl.styles.colors import Color
+from openpyxl.styles import PatternFill,Border,Side
+
+
+
+# This code is to hide the main tkinter window
+root = tkinter.Tk()
+root.withdraw()
+
+# Message Box
+messagebox.showinfo("Title", "Message")
+
+
+
+
+
+
+
+
+
+
+
+
+
